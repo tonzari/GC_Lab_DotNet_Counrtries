cb276f002c4c65f5450bb4a78012c4167edbee07
antonio <antoniomanzari@gmail.com>
2021-02-22 15:00:12 -0500

    init commit

---
+++ GC_Lab_DotNet_Countries/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_Lab_DotNet_Countries.Models
+{
+    public class Country
+    {
+    }
+}
---
+++ GC_Lab_DotNet_Countries/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GC_Lab_DotNet_Countries.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
