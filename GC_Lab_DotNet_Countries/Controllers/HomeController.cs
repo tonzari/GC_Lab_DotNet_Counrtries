9b25f7d2df51f1f60ba3186e59c82148dfbd4919
antonio <antoniomanzari@gmail.com>
2021-02-22 16:30:36 -0500

    made a list of countries to test

--- GC_Lab_DotNet_Countries/Controllers/HomeController.cs
+++ GC_Lab_DotNet_Countries/Controllers/HomeController.cs
@@ -6,16 +6,33 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GC_Lab_DotNet_Countries.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        public List<Country> Countries { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            Countries = new List<Country>();
+
+            Countries.Add(new Country(
+                "Mexico",
+                new string[] { "Spanish", "Nahuatl" },
+                "Que pedo guey?",
+                "There are many indigenous languages in Mexico, one the most popular being an Aztecan language commonly known as Nahuatl. You probably already know a couple Nahuatl words: chocolate and coyote! Some other nahuatl words that have spread across the world are: avocado, guacamole, and chili.",
+                new string[] { "Red", "White", "Green" }
+                ));
+
+            Countries.Add(new Country(
+                "Italy",
+                new string[] { "Italian" },
+                "Mi piacciono i tuoi pantaloni.",
+                "There are too people named 'Antonio' in Italy.",
+                new string[] { "Red", "White", "Green" }
+                ));
         }
 
         public IActionResult Index()
@@ -26,6 +43,12 @@ namespace GC_Lab_DotNet_Countries.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetCountry(string country)
+        {
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- GC_Lab_DotNet_Countries/Models/Country.cs
+++ GC_Lab_DotNet_Countries/Models/Country.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GC_Lab_DotNet_Countries.Models
 {
     public class Country
     {
+        public Country(string name, string[] languages, string greeting, string description, string[] colors)
+        {
+            Name = name;
+            Languages = languages;
+            Greeting = greeting;
+            Description = description;
+            Colors = colors;
+        }
+
+        public Country()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string[] Languages { get; set; }
+        public string Greeting { get; set; }
+        public string Description { get; set; }
+        public string[] Colors { get; set; }
     }
 }
